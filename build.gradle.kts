5124293d0a0ee5d851341be539784656ad84d052
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-13 15:14:07 +0100

    build: Specify JVM toolchain version 21 within kotlin extension function
    build: Specify JVM toolchain version 21 within kotlin extension {} function to give us a cleaner build.gradle.kts file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,25 +8,19 @@ plugins {
 group = "com.github"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
 repositories {
 	mavenCentral()
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
+	jvmToolchain(21)
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
