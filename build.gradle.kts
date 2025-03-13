6c204ef8de9e74aa8d89d6ab17bf42031f742b7d
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-13 15:22:48 +0100

    build: Define plugins using local version catalog
    Define plugins and versions in root build.gradle.kts file using the local version catalog.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version local.versions.kotlinVersion
+	kotlin("plugin.spring") version local.versions.kotlinVersion
+	alias(local.plugins.spring.boot)
+	alias(local.plugins.spring.dependencies)
 }
 
 group = "com.github"
