7641f2afa1db266ea7538b2d1a6e8053ba2b5c8d
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-13 18:49:34 +0100

    feat: Add basic error handling using @ControllerAdvice

---
+++ apps/api/src/main/kotlin/com/github/sample/exception/ControllerAdvisor.kt
@@ -0,0 +1,32 @@
+package com.github.sample.exception
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+/**
+ * Controller advisor for handling exceptions.
+ * This ensures that whenever an exception is thrown, a proper error response is returned to the client.
+ */
+@ControllerAdvice
+class ControllerAdvisor : ResponseEntityExceptionHandler() {
+
+    private val logger = LoggerFactory.getLogger(ControllerAdvisor::class.java)
+
+    /**
+     * Handles all exceptions.
+     * If any exception is thrown, this method will catch it and return a response entity with an [Error].
+     * @param ex The exception to handle.
+     * @return A response entity with an [Error]
+     */
+    @ExceptionHandler
+    fun handleEverything(ex: Exception): ResponseEntity<Error> {
+        val error = Error(
+            description = ex.message.toString(),
+        )
+        logger.error(ex.message, ex)
+        return ResponseEntity.internalServerError().body(error)
+    }
+}
