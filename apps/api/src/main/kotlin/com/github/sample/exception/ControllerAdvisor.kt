e34f88263bedae65018019cf3a22da4482e7d548
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-13 19:31:16 +0100

    refac: Add common function in ControllerAdvisor
    Add common function to log the exception and return a ResponseEntity<Error> which can be used for both domain exceptions and non-domain exceptions. Spring will automatically handle it so domain exceptions are correctly caught while all other exceptions will show a general error.

--- apps/api/src/main/kotlin/com/github/sample/exception/ControllerAdvisor.kt
+++ apps/api/src/main/kotlin/com/github/sample/exception/ControllerAdvisor.kt
@@ -1,6 +1,7 @@
 package com.github.sample.exception
 
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -18,31 +19,43 @@ class ControllerAdvisor : ResponseEntityExceptionHandler() {
     /**
      * Handles all domain exception.
      * If any [DomainException] is thrown, this method will catch it and return a response entity with an [Error].
-     * The returned HTTP code will be derived from the specific [DomainException].
-     * @param ex The domain exception to handle.
+     * The returned HTTP status code will be derived from the specific [DomainException].
+     * @param exception The domain exception to handle.
      * @return A response entity with an [Error]
      */
     @ExceptionHandler(value = [DomainException::class])
-    fun handleDomainException(ex: DomainException): ResponseEntity<Error> {
-        val error = Error(
-            description = ex.message.toString(),
+    fun handleDomainException(exception: DomainException): ResponseEntity<Error> {
+        return error(
+            exception = exception,
+            httpStatus = exception.httpStatus
         )
-        logger.error(ex.message, ex)
-        return ResponseEntity.status(ex.httpStatus.value()).body(error)
     }
 
     /**
      * Handles all exceptions.
      * If any exception is thrown, this method will catch it and return a response entity with an [Error].
-     * @param ex The exception to handle.
+     * Returns an HTTP 500 status code if no domain exception is thrown.
+     * @param exception The exception to handle.
      * @return A response entity with an [Error]
      */
     @ExceptionHandler
-    fun handleEverything(ex: Exception): ResponseEntity<Error> {
-        val error = Error(
-            description = ex.message.toString(),
+    fun handleEverything(exception: Exception): ResponseEntity<Error> {
+        return error(
+            exception = exception,
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
         )
-        logger.error(ex.message, ex)
-        return ResponseEntity.internalServerError().body(error)
+    }
+
+    /**
+     * Log exception and return a response entity with an [Error].
+     * @param exception [Exception]
+     * @param httpStatus [HttpStatus]
+     */
+    private fun error(exception: Exception, httpStatus: HttpStatus): ResponseEntity<Error> {
+        val error = Error(
+            description = exception.message.toString(),
+        )
+        logger.error(exception.message, exception)
+        return ResponseEntity.status(httpStatus.value()).body(error)
     }
 }
